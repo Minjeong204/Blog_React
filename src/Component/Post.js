085b0022417e51ecfc8055249aebc88cf80e0029
Minjeong204 <anjell1218@gmail.com>
2023-03-31 18:02:34 +0900

    text

--- src/Component/Post.js
+++ src/Component/Post.js
@@ -4,28 +4,32 @@ import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
 import { v4 as uuidv4 } from "uuid";
 import { Box } from "@mui/system";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { Button } from "@mui/material";
 
 function Post(props) {
     const { posts, title } = props;
-    const arr = [{ post: "", title: "" }];
-    console.log(posts[0]);
+    const arr = [];
     for (var i = 0; i < posts.length; i++) {
         arr.push({ post: posts[i], title: title[i] });
     }
-    arr.shift();
-    console.log(arr);
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <Grid
-            item
-            height={1000}
-            xs={12}
-            md={12}
-            sx={{
-                "& .markdown": {
-                    py: 3,
-                },
-            }}>
-            {arr.map((po) => (
+        <Grid item height={1000} xs={12} md={12}>
+            {arr.map((po) => {
                 <Box
                     sx={{
                         height: 400,
@@ -33,15 +37,40 @@ function Post(props) {
                         paddingLeft: 5,
                         paddingRight: 5,
                         marginBottom: 2,
-                    }}
-                    key={uuidv4()}>
-                    <h2 className="title" key={uuidv4()}>
-                        {po.title}
-                    </h2>
-                    <Divider key={uuidv4()} />
-                    <p key={uuidv4()}>{po.post}</p>
-                </Box>
-            ))}
+                    }}>
+                    <div>
+                        <h2 className="title" style={{ display: "inline-block" }}>
+                            {po.title}
+                        </h2>
+                        <Button
+                            id="basic-button"
+                            aria-controls={open ? "basic-menu" : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? "true" : undefined}
+                            onClick={handleClick}
+                            sx={{
+                                float: "right",
+                                marginTop: 2,
+                            }}>
+                            <MoreHorizIcon />
+                        </Button>
+                        <Menu
+                            id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                                "aria-labelledby": "basic-button",
+                            }}>
+                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </Menu>
+                    </div>
+                    <Divider />
+                    <p dangerouslySetInnerHTML={{ __html: po.post }}></p>
+                </Box>;
+            })}
         </Grid>
     );
 }
--- src/Page/Write.js
+++ src/Page/Write.js
@@ -32,7 +32,9 @@ const Writing = memo(({ api }) => {
 
     const handleSubmit = () => {
         const content = quillRef.current.getEditor().getContents(); //태그를 제외한 순수 text만을 받아온다. 검색기능을 구현하지 않을 거라면 굳이 text만 따로 저장할 필요는 없다.
-        const descriptions = quillRef.current.getEditor().getText(); //태그를 제외한 순수 text만을 받아온다. 검색기능을 구현하지 않을 거라면 굳이 text만 따로 저장할 필요는 없다.
+        const descriptions = quillRef.current.value; //태그를 제외한 순수 text만을 받아온다. 검색기능을 구현하지 않을 거라면 굳이 text만 따로 저장할 필요는 없다.
+        console.log(quillRef.current.value);
+        console.log(quillRef.current.getEditor());
         if (descriptions.trim() === "") {
             alert("내용을 입력해주세요.");
             return;
@@ -43,7 +45,7 @@ const Writing = memo(({ api }) => {
                 "/regipost",
                 {
                     title: title,
-                    content: content.ops[0].insert,
+                    content: descriptions,
                     category: category,
                     useYn: useYn,
                     regiUser: regi,
@@ -54,7 +56,7 @@ const Writing = memo(({ api }) => {
             )
             .then(function (response) {
                 alert("등록 성공");
-                document.location.href = "/blog";
+                // document.location.href = "/blog";
             })
             .catch(function (error) {
                 console.error(error);
